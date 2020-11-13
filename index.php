feb6364220307641025badc58a7ff49f64493969
Zemian Deng <zemiandeng@gmail.com>
2020-11-13 17:16:09 -0500

    Improve progress bar

--- index.php
+++ index.php
@@ -3,35 +3,35 @@
  * A simple php to browse a DocumentRoot directory where it list all dirs and files.
  *
  * SECURITY WARNING:
- * Exposing directory and files is consider security risk for publicly hosted server! This 
- * script is only intended for internal web site and serve as tool. 
+ * Exposing directory and files is consider security risk for publicly hosted server! This
+ * script is only intended for internal web site and serve as tool.
  *
  * Project Owner: Zemian Deng
  * Project Home: https://github.com/zemian/purple-index
  * License: The MIT License (MIT)
- * 
- * Dependencies: 
+ *
+ * Dependencies:
  *  - Bulma CSS (for UI styling) https://bulma.io/
  *  - Cloc (for dir stats) https://github.com/AlDanial/cloc
  *
- * Release Notes: 
- * 
+ * Release Notes:
+ *
  * 1.0.0 2020-11-04
  *  - List files alphabetically.
  *  - Each file should be listed as a link and go to the page when clicked.
  *  - List dir separately on the side as navigation.
- *  - Each dir is a link to browse sub dir content recursively. 
+ *  - Each dir is a link to browse sub dir content recursively.
  *  - Parent dir link
- * 
+ *
  * 1.1.0 2020-11-12
  *  - New style look with footer.
  *  - Navigation on links for each sub dir.
- * 
+ *
  * 1.1.1 2020-11-13
  *  - Remove unused parent parameter
  *  - Fix dir parameter to not have slash prefix
  *  - Fix error display
- * 
+ *
  * 1.2.0 2020-11-13
  *  - Add view dir stats link and output
  *  - Add AJAX to support for 'cloc' API for dir stats
@@ -70,8 +70,8 @@ if (isset($_GET['cloc'])) {
     } else {
         echo json_encode(array('error_message' => $error));
     }
-    
-    
+
+
     exit;
 }
 
@@ -81,7 +81,7 @@ $browse_dir = $_GET['dir'] ?? '';
 $dirs = [];
 $files = [];
 $url_path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-$dir_stat = isset($_GET['dir_stats']);
+$dir_stats = isset($_GET['dir_stats']);
 $list_path = ($browse_dir === '') ? $root_path : "$root_path/$browse_dir";
 $error = validate_path($list_path, $browse_dir);
 
@@ -101,7 +101,7 @@ if ($error === null) {
         }
     }
     $dir_nav_links = implode('/', $dir_links);
-    
+
     // We need to get rid of the first two entries for "." and ".." returned by scandir().
     $list = array_slice(scandir($list_path), 2);
     foreach ($list as $item) {
@@ -130,8 +130,11 @@ if ($error === null) {
         .tag.has-purple-bg {
             background-color: hsl(294, 71%, 79%);
         }
-        .progress.has-purple-bg {
+        .progress.is-extra-small {
+            height: 2px;
+            color: inherit;
             background-color: hsl(294, 71%, 79%);
+            background-image: linear-gradient(to right, hsl(294, 71%, 79%) 30%, #ededed 30%);
         }
     </style>
 </head>
@@ -169,12 +172,12 @@ if ($error === null) {
         <div class="columns">
             <div class="column is-one-third" >
                 <!-- List of Directories -->
-                <div class="menu p-2" style="min-height: 60vh;"> 
+                <div class="menu p-2" style="min-height: 60vh;">
                     <?php // Bulma menu-label always capitalize words, so we override it to not do that for dir name sake. ?>
                     <p class="menu-label" style="text-transform: inherit;"><a href="<?php echo $url_path; ?>">Directory:</a> <?php echo $dir_nav_links; ?></p>
                     <ul class="menu-list">
                         <?php foreach ($dirs as $item) { ?>
-                        <li><a href="index.php?dir=<?php echo ($browse_dir === '') ? $item : "$browse_dir/$item"; ?>"><?php echo $item; ?></a></li>
+                            <li><a href="index.php?dir=<?php echo ($browse_dir === '') ? $item : "$browse_dir/$item"; ?>"><?php echo $item; ?></a></li>
                         <?php } ?>
                     </ul>
                 </div>
@@ -193,24 +196,24 @@ if ($error === null) {
             </div>
         </div> <!-- end of columns -->
 
-        <?php if ($dir_stat) { ?>
-            <div id='dir-stat'">
+        <?php if ($dir_stats) { ?>
+            <div id='dir-stats'>
                 <h1 class="subtitle has-text-centered">Directory Statistic
                     <span class="help">(provided by <a href="https://github.com/AlDanial/cloc">cloc</a>)</span>
                 </h1>
-                <progress id="dir-stat-progress" class="progress is-small has-purple-bg" max="100"></progress>
+                <progress id="dir-stats-progress" class="progress is-extra-small" max="100"></progress>
                 <div class="level">
                     <div class="level-item">
                         <pre id="cloc-output" style="visibility: hidden;"></pre>
                     </div>
                 </div>
             </div>
-            <div id='dir-stat-error' style="visibility: hidden;" class="has-text-centered">
-                <p>There seems to be a problem with the <code>cloc</code> command tool. 
+            <div id='dir-stats-error' style="visibility: hidden;" class="has-text-centered">
+                <p>There seems to be a problem with the <code>cloc</code> command tool.
                     Have you installed it? Try <code>brew install clock</code> if you are on a MacOSX.</p>
             </div>
         <?php } ?>
-        
+
     <?php } ?>
 </div> <!-- end of section -->
 
@@ -218,28 +221,28 @@ if ($error === null) {
     <div class="level">
         <div class="level-item has-text-centered">
             <div>
-            <p>Powered by <a href="https://github.com/zemian/purple-index">purple-index</a></p> 
-            <?php if (!$dir_stat) {
-                echo "<p></p><a href='$url_path?dir=$browse_dir&dir_stats'>view dir stats</a></p>";    
-            } ?>
+                <p>Powered by <a href="https://github.com/zemian/purple-index">purple-index</a></p>
+                <?php if (!$dir_stats) {
+                    echo "<p></p><a href='$url_path?dir=$browse_dir&dir_stats'>view dir stats</a></p>";
+                } ?>
             </div>
         </div>
     </div>
 </div>
 
-<?php if ($dir_stat) { ?>
+<?php if ($dir_stats) { ?>
     <script>
         var url = '<?php echo "$url_path?cloc&dir=$browse_dir" ?>';
         document.addEventListener('DOMContentLoaded', function () {
             fetch(url).then(resp => resp.json()).then(data => {
-                document.getElementById('dir-stat-progress').style.visibility = 'hidden';
+                document.getElementById('dir-stats-progress').style.visibility = 'hidden';
                 if (!data.error_message) {
                     var el = document.getElementById('cloc-output');
                     el.innerText = data.output;
                     el.style.visibility = 'visible';
                 } else {
                     console.warn('Fetch failed: ' + data.error_message);
-                    document.getElementById('dir-stat-error').style.visibility = 'visible';
+                    document.getElementById('dir-stats-error').style.visibility = 'visible';
                 }
             });
         });
