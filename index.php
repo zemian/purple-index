21f1b56ada0f494f6bbb0e51fccf7884c4b906e1
Zemian Deng <zemiandeng@gmail.com>
2020-11-12 23:31:32 -0500

    Add links to dir paths and improving looks

--- index.php
+++ index.php
@@ -24,6 +24,22 @@ $parent_browse_dir = $_GET['parent'] ?? '';
 $error = '';
 $dirs = [];
 $files = [];
+$url = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+// Build dir navigation
+$dir_links = explode('/', $browse_dir);
+$dir_links_len = count($dir_links);
+$dir_paths = [];
+$dir_links_idx = 1;
+foreach ($dir_links as &$dir) {
+    $parent_path = implode('/', $dir_paths);
+    $path = "$parent_path/$dir";
+    $dir_paths []= $dir;
+    if ($dir_links_idx++ < $dir_links_len) { // Update all except last element
+        $dir = "<a href='$url?dir=$path&parent=$parent_path'>$dir</a>"; // Update by ref!
+    }
+}
+$browse_dir_url = implode('/', $dir_links);
 
 // Internal vars
 $root_path = __DIR__;
@@ -61,26 +77,26 @@ if (!$error) {
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://unpkg.com/bulma">
-    <title><?= $title ?></title>
+    <title><?php echo $title; ?></title>
 </head>
 <body>
 <div class="section">
     <div class="level">
-        <div class="level-item">
-            <a href="index.php"><h1 class="title"><?= $title ?></h1></a>
+        <div class="level-left">
+            <div class="level-item">
+                <h1 class="title"><?php echo $title; ?></h1>
+            </div>
         </div>
     </div>
-    <div class="columns">
-        <div class="column is-one-third">
+    <div class="columns has-background-light">
+        <div class="column is-one-third" style="min-height: 80vh;">
             <!-- List of Directories -->
-            <div class="menu">                
-                <p class="menu-label">Directory: <?= $browse_dir; ?></p>
+            <div class="menu">       
+                <?php // Bulma menu-label always capitalize words, so we override it to not do that for dir name sake. ?>
+                <p class="menu-label" style="text-transform: inherit;"><a href="<?php echo $url; ?>">Directory:</a> <?php echo $browse_dir_url; ?></p>
                 <ul class="menu-list">
-                    <?php if ($browse_dir) { ?>
-                        <li><a href="index.php?dir=<?= $parent_browse_dir ?>">..</a></li>
-                    <?php } ?>
                     <?php foreach ($dirs as $item) { ?>
-                    <li><a href="index.php?dir=<?= "$browse_dir/$item" ?>&parent=<?= $browse_dir ?>"><?= $item ?></a></li>
+                    <li><a href="index.php?dir=<?php echo "$browse_dir/$item"; ?>&parent=<?php echo $browse_dir; ?>"><?php echo $item; ?></a></li>
                     <?php } ?>
                 </ul>
             </div>
@@ -88,20 +104,21 @@ if (!$error) {
         <div class="column">
             <?php if ($error) { ?>
                 <div class="notification is-danger">
-                    <?= $error ?>
+                    <?php echo $error; ?>
                 </div>
             <?php } else { ?>
                 <!-- List of Files -->
-                <div class="content">
-                    <ul>
-                        <?php foreach ($files as $item) { ?>
-                            <li><a href="<?= "$browse_dir/$item" ?>"><?= $item ?></a></li>
-                        <?php } ?>
-                    </ul>
-                </div>
+                <ul class="panel has-background-white">
+                    <?php foreach ($files as $item) { ?>
+                        <li class="panel-block"><a href="<?php echo "$browse_dir/$item"; ?>"><?php echo $item; ?></a></li>
+                    <?php } ?>
+                </ul>
             <?php } ?>
         </div>
     </div>
+</div>
+<div class="footer has-background-white">
+    Powered by <a href="https://github.com/zemian/index-listing">index-listing</a>.
 </div>
 
 </body>
